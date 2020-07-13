5b44b870318666c3b8ffe4cdd8980a47335ea942
tia9orodri <48485537+tia9orodri@users.noreply.github.com>
2020-07-13 19:09:16 +0100

    Faltas na Autenticação

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -7,6 +7,7 @@ exports.register = (req, res) => {
         .then(() => res.sendStatus(200))
         .catch((message) => res.status(500).send(message));
 };
+
 exports.login = (req, res) => {
     userService
         .authenticate(req.body.username, req.body.password)
@@ -14,3 +15,24 @@ exports.login = (req, res) => {
         .then((data) => res.json(data))
         .catch((error) => res.status(500).send(error.message));
 };
+
+exports.getAnimais = (req, res) => {
+    userService
+      .getAnimais(req.client, req.query)
+      .then((result) => res.json(result))
+      .catch((err) => res.status(500).send(err.message));
+  };
+
+  exports.addAnimal = (req, res) => {
+    userService
+      .addAnimal(req.client, req.params.id)
+      .then(() => res.json({ success: true }))
+      .catch((err) => res.status(500).send(err.message));
+  };
+
+  exports.removeAnimal = (req, res) => {
+    userService
+      .removeAnimal(req.client, req.params.id)
+      .then(() => res.json({ success: true }))
+      .catch((err) => res.status(500).send(err.message));
+  };
